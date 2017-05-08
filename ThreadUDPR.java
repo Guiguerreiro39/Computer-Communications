1b0294f7047dfb08493704f4738b79fd8f7912f3
ricardocerto16 <a75315@alunos.uminho.pt>
2017-05-08 09:56:14 +0100

    add Threads UDP

---
+++ ThreadUDPR.java
@@ -0,0 +1,13 @@
+package tpcc;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.net.DatagramSocket;
+import java.net.DatagramPacket;
+
+public class ThreadUDPR(){
+	// thread UDP responsável por efetuar 
+	// a leitura e escrever 
+
+
+}
